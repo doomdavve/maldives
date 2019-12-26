9f51aed6976bdcbca1b9da5ed5fda8b09a4ca02d
David Vest <davve@davve.net>
2019-12-26 17:14:02 +0100

    Output error with prefix and use modules

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 mod token;
 mod lexer;
 
+use token::TokenType;
+use lexer::Lexer;
+
 use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("main.ur")
         .expect("Something went wrong reading the file");
 
-    let mut lexer = lexer::Lexer::new(&contents);
+    let mut lexer = Lexer::new(&contents);
     while let Some(token) = lexer.next() {
-        println!("{:?}", token);
-        if token.token_type == token::TokenType::Error {
+        if token.token_type == TokenType::Error {
+            println!("Error: {:?}", token);
             break;
         }
+        println!("{:?}", token);
     }
 }
