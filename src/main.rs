651a486041af08af8991a8a3587656e416333a22
David Vest <davve@davve.net>
2019-12-26 14:08:04 +0100

    Add whitepace tokenization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::str::from_utf8_unchecked;
 
 struct Lexer<'a> {
-    cur: &'a [u8]
+    cur: &'a [u8],
 }
 
 fn eat_digits<'a>(s: &'a [u8]) -> (&[u8], &'a [u8]) {
@@ -12,10 +12,18 @@ fn eat_digits<'a>(s: &'a [u8]) -> (&[u8], &'a [u8]) {
     (&s[..i], &s[i..])
 }
 
+fn eat_whitespace<'a>(s: &'a [u8]) -> (&[u8], &'a [u8]) {
+    let mut i = 0;
+    while i < s.len() && b' ' == s[i] {
+        i += 1;
+    }
+    (&s[..i], &s[i..])
+}
+
 impl<'a> Lexer<'a> {
     fn new(buffer: &'a str) -> Lexer<'a> {
         Lexer {
-            cur: buffer.as_bytes()
+            cur: buffer.as_bytes(),
         }
     }
 
@@ -23,28 +31,52 @@ impl<'a> Lexer<'a> {
         let (number, trailing) = eat_digits(self.cur);
         if !number.is_empty() {
             unsafe {
-                let token = from_utf8_unchecked(number).parse::<i32>().map(|j| Token::integer(j)).ok();
+                let token = from_utf8_unchecked(number)
+                    .parse::<i32>()
+                    .map(|j| Token::integer(j))
+                    .ok();
                 (token, trailing)
             }
         } else {
             (None, trailing)
         }
     }
+
+    fn tokenize_whitespace(&self) -> (Option<Token>, &'a [u8]) {
+        let (whitespace, trailing) = eat_whitespace(self.cur);
+        if !whitespace.is_empty() {
+            (Some(Token::whitespace()), trailing)
+        } else {
+            (None, trailing)
+        }
+    }
+
+}
+
+macro_rules! tokenize {
+    ($self:ident, $func:expr) => {{
+        let (token, _rest) = $func;
+        $self.cur = _rest;
+        if token.is_some() {
+            return token;
+        }
+    }};
 }
 
 impl<'a> Iterator for Lexer<'a> {
     type Item = Token;
     fn next(&mut self) -> Option<Token> {
         // Attempt to parse as number first.
-        let (token, _rest) = self.tokenize_number();
-        self.cur = _rest;
-        token
+        tokenize!(self, self.tokenize_number());
+        tokenize!(self, self.tokenize_whitespace());
+        None
     }
 }
 
 #[derive(Debug)]
 enum TokenType {
     Integer,
+    Whitespace
 }
 
 #[derive(Debug)]
@@ -60,10 +92,16 @@ impl Token {
             integer_value: integer,
         }
     }
+    fn whitespace() -> Token {
+        Token {
+            token_type: TokenType::Whitespace,
+            integer_value: 0,
+        }
+    }
 }
 
 fn main() {
-    let test: &str = "223 123";
+    let test: &str = "223 123 1 2 4 4 5  6  7 7456  345  45";
     let mut lexer = Lexer::new(test);
     while let Some(token) = lexer.next() {
         println!("{:?}", token);
