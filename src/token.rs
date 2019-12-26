b9a4c495a7f199b389a8b98ed5da1f1e643c7727
David Vest <davve@davve.net>
2019-12-26 17:08:06 +0100

    Factor our token and lexer code into modules

---
+++ src/token.rs
@@ -0,0 +1,70 @@
+#[derive(Debug)]
+pub enum Keyword {
+    Function,
+    BraceLeft,
+    BraceRight,
+    ParanLeft,
+    ParanRight,
+    Comma
+}
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum TokenType {
+    Integer,
+    Whitespace,
+    Symbol,
+    Keyword,
+    Error
+}
+
+#[derive(Debug)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub integer_value: i32,
+    pub symbol: Option<String>,
+    pub keyword: Option<Keyword>
+}
+
+impl Token {
+    pub fn integer(integer: i32) -> Token {
+        Token {
+            token_type: TokenType::Integer,
+            integer_value: integer,
+            symbol: None,
+            keyword: None
+        }
+    }
+    pub fn whitespace() -> Token {
+        Token {
+            token_type: TokenType::Whitespace,
+            integer_value: 0,
+            symbol: None,
+            keyword: None
+        }
+    }
+    pub fn symbol(sym: &str) -> Token {
+        Token {
+            token_type: TokenType::Symbol,
+            integer_value: 0,
+            symbol: Some(sym.to_string()),
+            keyword: None
+        }
+    }
+    pub fn keyword(keyword: Keyword) -> Token {
+        Token {
+            token_type: TokenType::Keyword,
+            integer_value: 0,
+            symbol: None,
+            keyword: Some(keyword)
+        }
+    }
+    pub fn error() -> Token {
+        Token {
+            token_type: TokenType::Error,
+            integer_value: 0,
+            symbol: None,
+            keyword: None
+        }
+    }
+}
