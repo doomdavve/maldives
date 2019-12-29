96a4923838cecc3fb592270b23704e45c658d0c5
David Vest <davve@davve.net>
2019-12-29 16:03:26 +0100

    Remove disabled code

--- src/parser.rs
+++ src/parser.rs
@@ -10,79 +10,9 @@ use std::str::from_utf8_unchecked;
 
 use crate::token::Token;
 
-//fn parse_expression(tokens: &Vec<Token>, pos: usize) -> Option<TreeNode> {
-//    let (node, _next) = parse_function_call(tokens, pos);
-//    node
-//}
-//
-//fn parse_function_call(tokens: &Vec<Token>, pos: usize) -> (Option<TreeNode>, usize) {
-//    let (symbol, next) = parse_symbol(tokens, pos);
-//    (match symbol {
-//        Some(name) => {
-//            let (arguments, rest) = parse_arguments(tokens, next);
-//            (Some(TreeNode {
-//                name: name,
-//                children: arguments,
-//            }), rest)
-//        },
-//        _ => (None, next),
-//    })
-//}
-//
-//fn accept_token(tokens: &Vec<Token>, pos: usize, token: Token) -> Option<usize> {
-//    return tokens.get(pos).and_then(|s| {
-//        if *s == token {
-//            Some(pos + 1)
-//        } else {
-//            None
-//        }
-//    });
-//}
-//
-//fn parse_arguments(tokens: &Vec<Token>, pos: usize) -> (Vec<TreeNode>, usize) {
-//    match accept_token(tokens, pos, Token::keyword(Keyword::ParenLeft)) {
-//        Some(argument_start) => {
-//            loop {
-//                let next = accept_token(tokens, pos, Token::keyword(Keyword::ParenRight));
-//                if (next.is_some())
-//                    ;
-//
-//                let token = tokens.get(pos).and_then(|s| {
-//                    if s.token_type == TokenType::Keyword && s.keyword == Some(Keyword::ParenRight) {
-//                        Some(s)
-//                    } else {
-//                        None
-//                    }
-//                });
-//                if token.is_some() {
-//                    break;
-//                }
-//            }
-//        },
-//        None => (vec!(), pos)
-//    }
-////
-////    let token = tokens.get(pos).and_then(|s| {
-////        if s.token_type == TokenType::Keyword && s.keyword == Some(Keyword::ParenLeft) {
-////            Some(s)
-////        } else {
-////            None
-////        }
-////    });
-////
-////    if token.is_none() {
-////        return ;
-////    }
-//}
-
 #[derive(Debug, Clone, PartialEq)]
 pub struct ParserError;
 
-// Generation of an error is completely separate from how it is displayed.
-// There's no need to be concerned about cluttering complex logic with the display style.
-//
-// Note that we don't store any extra info about the errors. This means we can't state
-// which string failed to parse without modifying our types to carry that information.
 impl fmt::Display for ParserError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Parser error")
