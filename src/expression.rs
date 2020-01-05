5c25b352f457d19199435f7ca9655595575783c6
David Vest <davve@davve.net>
2020-01-05 16:57:21 +0100

    Type system: Start by resolving the easy cases

--- src/expression.rs
+++ src/expression.rs
@@ -1,4 +1,31 @@
 use std::rc::Rc;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+pub enum PrimitiveType {
+    Integer,
+    Bool,
+    String,
+    Function,
+    Any,
+    None
+}
+
+impl FromStr for PrimitiveType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<PrimitiveType, ()> {
+        match s {
+            "int" => Ok(PrimitiveType::Integer),
+            "bool" => Ok(PrimitiveType::Bool),
+            "string" => Ok(PrimitiveType::String),
+            "fn" => Ok(PrimitiveType::Function),
+            "any" => Ok(PrimitiveType::Any),
+            "none" => Ok(PrimitiveType::None),
+            _ => Err(()),
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expression {
@@ -16,8 +43,28 @@ pub enum Expression {
     Void,
 }
 
+impl Expression {
+    // Should we pass in the symbol table here? Which one? :)
+    pub fn resolve_type(&self) -> PrimitiveType {
+        match self {
+            Expression::Integer(_) => PrimitiveType::Integer,
+            Expression::String(_) => PrimitiveType::String,
+            Expression::Bool(_) => PrimitiveType::Bool,
+            Expression::Function(_) => PrimitiveType::Function,
+            Expression::Symbol(_) => PrimitiveType::Any, // Need the symbol table to tell
+            Expression::Bind(bind) => bind.expr.resolve_type(),
+            Expression::Block(block) => block.list.last().map(|e| e.resolve_type()).unwrap_or(PrimitiveType::None),
+            Expression::FunctionCall(fc) => fc.expr.resolve_type(), // TODO: here we have work to do.
+            Expression::Binary(binary) => binary.operation.resolve_type(&binary.left, &binary.right),
+            Expression::Group(group) => group.expr.resolve_type(),
+            Expression::Conditional(_) => PrimitiveType::Any, // Check both braches and assert they are the same
+            Expression::Void => PrimitiveType::None
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
-pub enum Operation {
+pub enum Operation { // call it BinaryOperation
     Sum,
     Difference,
     Multiply,
@@ -25,7 +72,22 @@ pub enum Operation {
     LessThan,
     GreaterThan,
     LessEqualThan,
-    GreaterEqualThan
+    GreaterEqualThan,
+}
+
+impl Operation {
+    // Should we pass in the symbol table here? Which one? :)
+    pub fn resolve_type(&self, left: &Expression, right: &Expression) -> PrimitiveType {
+        match self {
+            Operation::Sum => if left.resolve_type() == right.resolve_type() {
+                left.resolve_type()
+            } else {
+                PrimitiveType::Any
+            }
+            Operation::Difference | Operation::Multiply | Operation::Divide => PrimitiveType::Integer,
+            Operation::LessThan | Operation::GreaterThan | Operation::LessEqualThan | Operation::GreaterEqualThan => PrimitiveType::Bool
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
