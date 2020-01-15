39a3b6416f7a1180e4b5a7b231dd61ed0c8d8c6c
David Vest <davve@davve.net>
2020-01-15 17:26:30 +0100

    Restructure tests in main into a tests module
    Apparently, that is the convention to use. It gives redundant info in
    the output so I'm not totally sold.

--- src/main.rs
+++ src/main.rs
@@ -27,11 +27,14 @@ mod typeresolver;
 
 use interpreter::Interpreter;
 use lexer::Lexer;
+use native::native_dbg;
+use native::native_println;
 use parser::Parser;
 use resolvedtype::ResolvedType;
 use symboltable::Closure;
 use symboltable::SymbolTable;
 use typedexpression::{TypedExpression, TypedExpressionNode};
+use typeresolver::TypeResolver;
 
 fn main() -> Result<(), String> {
     env_logger::init();
@@ -142,163 +145,171 @@ fn repl() -> Result<i32, String> {
 }
 
 #[cfg(test)]
-use std::rc::Rc;
+mod tests {
+    use std::rc::Rc;
 
-#[cfg(test)]
-use typeresolver::TypeResolverError;
+    use crate::interpreter::Interpreter;
+    use crate::lexer::Lexer;
+    use crate::parser::Parser;
+    use crate::resolvedtype::ResolvedFunctionType;
+    use crate::resolvedtype::ResolvedType;
+    use crate::symboltable::Closure;
+    use crate::symboltable::SymbolTable;
+    use crate::typedexpression::TypedExpression;
+    use crate::typeresolver::TypeResolver;
+    use crate::typeresolver::TypeResolverError;
 
-#[test]
-fn eval_simple() {
-    let mut parser = Parser::new(Lexer::new("apa"));
-    let mut root = SymbolTable::new();
-    root.bind(
-        "apa".to_string(),
-        Closure::simple(TypedExpression::integer(4)),
-    );
-    let expression = TypeResolver::resolve_in_env(&parser.program().unwrap(), &mut root).unwrap();
-    let res = Interpreter::eval_expression(&expression, &mut root);
-    assert_eq!(Ok(TypedExpression::integer(4)), res);
-}
+    fn type_check_program(
+        contents: &str,
+        mut root: &SymbolTable,
+    ) -> Result<TypedExpression, TypeResolverError> {
+        let mut parser = Parser::new(Lexer::new(contents));
+        TypeResolver::resolve_in_env(&parser.program().unwrap(), &mut root)
+    }
 
-#[cfg(test)]
-fn type_check_program(
-    contents: &str,
-    mut root: &SymbolTable,
-) -> Result<TypedExpression, TypeResolverError> {
-    let mut parser = Parser::new(Lexer::new(contents));
-    TypeResolver::resolve_in_env(&parser.program().unwrap(), &mut root)
-}
+    fn eval_program(contents: &str) -> Result<TypedExpression, ()> {
+        let mut root = SymbolTable::new();
+        let expression = type_check_program(contents, &mut root).map_err(|_| ())?;
+        Interpreter::eval_expression(&expression, &mut root).map_err(|_| ())
+    }
 
-#[cfg(test)]
-fn eval_program(contents: &str) -> Result<TypedExpression, ()> {
-    let mut root = SymbolTable::new();
-    let expression = type_check_program(contents, &mut root).map_err(|_| ())?;
-    Interpreter::eval_expression(&expression, &mut root).map_err(|_| ())
-}
+    #[test]
+    fn eval_simple() {
+        let mut parser = Parser::new(Lexer::new("apa"));
+        let mut root = SymbolTable::new();
+        root.bind(
+            "apa".to_string(),
+            Closure::simple(TypedExpression::integer(4)),
+        );
+        let expression =
+            TypeResolver::resolve_in_env(&parser.program().unwrap(), &mut root).unwrap();
+        let res = Interpreter::eval_expression(&expression, &mut root);
+        assert_eq!(Ok(TypedExpression::integer(4)), res);
+    }
 
-#[test]
-fn eval_simple_assignment() {
-    assert_eq!(
-        Ok(TypedExpression::integer(3)),
-        eval_program("{ let apa = 3; apa }")
-    );
-}
+    #[test]
+    fn eval_simple_assignment() {
+        assert_eq!(
+            Ok(TypedExpression::integer(3)),
+            eval_program("{ let apa = 3; apa }")
+        );
+    }
 
-#[test]
-fn eval_assignment() {
-    assert_eq!(Ok(TypedExpression::integer(3)), eval_program("let apa = 3"));
-}
+    #[test]
+    fn eval_assignment() {
+        assert_eq!(Ok(TypedExpression::integer(3)), eval_program("let apa = 3"));
+    }
 
-#[test]
-fn eval_anon_function() {
-    assert_eq!(
-        Ok(TypedExpression::integer(3)),
-        eval_program("{ let apa = fn() -> int = 3; apa() }")
-    );
-}
+    #[test]
+    fn eval_anon_function() {
+        assert_eq!(
+            Ok(TypedExpression::integer(3)),
+            eval_program("{ let apa = fn() -> int = 3; apa() }")
+        );
+    }
 
-#[test]
-fn eval_anon_function_with_arg() {
-    assert_eq!(
-        Ok(TypedExpression::integer(3)),
-        eval_program("{ let apa = fn(x: int) -> int = x; apa(3) }")
-    );
-}
+    #[test]
+    fn eval_anon_function_with_arg() {
+        assert_eq!(
+            Ok(TypedExpression::integer(3)),
+            eval_program("{ let apa = fn(x: int) -> int = x; apa(3) }")
+        );
+    }
 
-#[test]
-fn eval_anon_function_return_function() {
-    assert_eq!(
-        Ok(TypedExpression::integer(10)),
-        eval_program("{ let f = fn() -> () -> int = fn () -> int = 10; f()() }")
-    );
-}
+    #[test]
+    fn eval_anon_function_return_function() {
+        assert_eq!(
+            Ok(TypedExpression::integer(10)),
+            eval_program("{ let f = fn() -> () -> int = fn () -> int = 10; f()() }")
+        );
+    }
 
-#[test]
-fn eval_anon_function_as_arg() {
-    assert_eq!(
-        Ok(TypedExpression::integer(6)),
-        eval_program(
-            "{ let apply = fn (x: (int) -> int, arg: int) -> int = x(arg); let doubler = fn (i: int) -> int = i*2; apply(doubler, 3) }"
-        )
-    );
-}
+    #[test]
+    fn eval_anon_function_as_arg() {
+        assert_eq!(
+            Ok(TypedExpression::integer(6)),
+            eval_program(
+                "{ let apply = fn (x: (int) -> int, arg: int) -> int = x(arg); let doubler = fn (i: int) -> int = i*2; apply(doubler, 3) }"
+            )
+        );
+    }
 
-#[test]
-fn eval_infix() {
-    assert_eq!(Ok(TypedExpression::integer(5)), eval_program("3 + 2"));
-}
+    #[test]
+    fn eval_infix() {
+        assert_eq!(Ok(TypedExpression::integer(5)), eval_program("3 + 2"));
+    }
 
-#[test]
-fn eval_infix_in_succession() {
-    assert_eq!(Ok(TypedExpression::integer(15)), eval_program("3 + 2 + 10"));
-}
+    #[test]
+    fn eval_infix_in_succession() {
+        assert_eq!(Ok(TypedExpression::integer(15)), eval_program("3 + 2 + 10"));
+    }
 
-#[test]
-fn eval_infix_operators_grouping() {
-    assert_eq!(
-        Ok(TypedExpression::integer(60)),
-        eval_program("(3 + 3) * 10")
-    );
-}
+    #[test]
+    fn eval_infix_operators_grouping() {
+        assert_eq!(
+            Ok(TypedExpression::integer(60)),
+            eval_program("(3 + 3) * 10")
+        );
+    }
 
-#[test]
-fn eval_infix_division() {
-    assert_eq!(
-        Ok(TypedExpression::integer(545)),
-        eval_program("125895 / 231")
-    );
-}
+    #[test]
+    fn eval_infix_division() {
+        assert_eq!(
+            Ok(TypedExpression::integer(545)),
+            eval_program("125895 / 231")
+        );
+    }
 
-#[test]
-fn eval_closure_1() {
-    assert_eq!(
-        Ok(TypedExpression::integer(12)),
-        eval_program("{ fn b() -> () -> int = { let a = 12; fn () -> int = a }; b()() }")
-    );
-}
+    #[test]
+    fn eval_closure_1() {
+        assert_eq!(
+            Ok(TypedExpression::integer(12)),
+            eval_program("{ fn b() -> () -> int = { let a = 12; fn () -> int = a }; b()() }")
+        );
+    }
 
-#[test]
-fn eval_closure_2() {
-    assert_eq!(
-        Ok(TypedExpression::integer(12)),
-        eval_program(
-            "{ let a = 1; fn b() -> () -> int = { let a = 12; fn () -> int = a }; b()() }"
-        )
-    );
-}
+    #[test]
+    fn eval_closure_2() {
+        assert_eq!(
+            Ok(TypedExpression::integer(12)),
+            eval_program(
+                "{ let a = 1; fn b() -> () -> int = { let a = 12; fn () -> int = a }; b()() }"
+            )
+        );
+    }
 
-#[test]
-fn eval_closure_3() {
-    assert_eq!(
-        Ok(TypedExpression::integer(12)),
-        eval_program(
-            "{ let a = 1; fn b() -> () -> int = { let a = 12; fn () -> int = a }; b()() }"
-        )
-    );
-}
+    #[test]
+    fn eval_closure_3() {
+        assert_eq!(
+            Ok(TypedExpression::integer(12)),
+            eval_program(
+                "{ let a = 1; fn b() -> () -> int = { let a = 12; fn () -> int = a }; b()() }"
+            )
+        );
+    }
 
-#[test]
-fn eval_string_concatenation() {
-    assert_eq!(
-        Ok(TypedExpression::string("apabanan".to_string())),
-        eval_program("{ let a = \"apa\"; let b = \"banan\"; a + b }")
-    );
-}
+    #[test]
+    fn eval_string_concatenation() {
+        assert_eq!(
+            Ok(TypedExpression::string("apabanan".to_string())),
+            eval_program("{ let a = \"apa\"; let b = \"banan\"; a + b }")
+        );
+    }
 
-#[test]
-fn test_check_operator_precedence() {
-    assert_eq!(
-        Ok(TypedExpression::bool(true)),
-        eval_program("77 == 1 + 8**2 + 2 * (3 + 3)")
-    );
-}
+    #[test]
+    fn test_check_operator_precedence() {
+        assert_eq!(
+            Ok(TypedExpression::bool(true)),
+            eval_program("77 == 1 + 8**2 + 2 * (3 + 3)")
+        );
+    }
 
-#[test]
-fn test_check_loop_and_break() {
-    assert_eq!(
-        Ok(TypedExpression::integer(46368)),
-        eval_program(
-            "let a = 1;
+    #[test]
+    fn test_check_loop_and_break() {
+        assert_eq!(
+            Ok(TypedExpression::integer(46368)),
+            eval_program(
+                "let a = 1;
              let b = 1;
              loop {
                  let c = a;
@@ -308,90 +319,86 @@ fn test_check_loop_and_break() {
                      break a
                  }
              }"
-        )
-    );
-}
+            )
+        );
+    }
 
-#[test]
-fn test_assignment() {
-    assert_eq!(
-        Ok(TypedExpression::integer(2)),
-        eval_program("{ let a = 1; a = 2; a }")
-    );
-}
+    #[test]
+    fn test_assignment() {
+        assert_eq!(
+            Ok(TypedExpression::integer(2)),
+            eval_program("{ let a = 1; a = 2; a }")
+        );
+    }
 
-#[test]
-fn test_assignment_error_type_mismatch() {
-    assert_eq!(
-        Err(()),
-        eval_program("{ let a = 1; a = \"some string\"; a }")
-    );
-}
+    #[test]
+    fn test_assignment_error_type_mismatch() {
+        assert_eq!(
+            Err(()),
+            eval_program("{ let a = 1; a = \"some string\"; a }")
+        );
+    }
 
-#[test]
-fn test_assignment_error_unbound() {
-    assert_eq!(Err(()), eval_program("{ a = \"some string\"; a }"));
-}
+    #[test]
+    fn test_assignment_error_unbound() {
+        assert_eq!(Err(()), eval_program("{ a = \"some string\"; a }"));
+    }
 
-use crate::native::native_dbg;
-use crate::native::native_println;
-use crate::typeresolver::TypeResolver;
-#[cfg(test)]
-use resolvedtype::ResolvedFunctionType;
+    #[test]
+    fn type_check_simple_integer() {
+        let res = type_check_program("10", &SymbolTable::new()).unwrap();
+        assert_eq!(ResolvedType::Integer, res.resolved_type);
+    }
 
-#[test]
-fn type_check_simple_integer() {
-    let res = type_check_program("10", &SymbolTable::new()).unwrap();
-    assert_eq!(ResolvedType::Integer, res.resolved_type);
-}
+    #[test]
+    fn type_check_simple_string() {
+        let res = type_check_program("\"apa\"", &SymbolTable::new()).unwrap();
+        assert_eq!(ResolvedType::String, res.resolved_type);
+    }
 
-#[test]
-fn type_check_simple_string() {
-    let res = type_check_program("\"apa\"", &SymbolTable::new()).unwrap();
-    assert_eq!(ResolvedType::String, res.resolved_type);
-}
+    #[test]
+    fn type_check_simple_bool() {
+        let res = type_check_program("true", &SymbolTable::new()).unwrap();
+        assert_eq!(ResolvedType::Bool, res.resolved_type);
+    }
 
-#[test]
-fn type_check_simple_bool() {
-    let res = type_check_program("true", &SymbolTable::new()).unwrap();
-    assert_eq!(ResolvedType::Bool, res.resolved_type);
-}
-
-#[test]
-fn type_check_function() {
-    let res = type_check_program("let a = fn() -> int = 10", &SymbolTable::new()).unwrap();
-    let expected = ResolvedType::Function(Rc::new(ResolvedFunctionType {
-        return_type: ResolvedType::Integer,
-        parameters: Vec::new(),
-    }));
-    assert_eq!(expected, res.resolved_type);
-}
-
-#[test]
-fn type_check_function_call() {
-    let res = type_check_program("{ let a = fn() -> int = 10; a() }", &SymbolTable::new()).unwrap();
-    assert_eq!(ResolvedType::Integer, res.resolved_type);
-}
-
-#[test]
-fn type_check_function_call_returning_function() {
-    let res = type_check_program(
-        "{ fn make_identity_fn() -> (int) -> int = fn(x: int) -> int = x }",
-        &SymbolTable::new(),
-    )
-    .unwrap();
-    let expected = ResolvedType::Function(Rc::new(ResolvedFunctionType {
-        return_type: ResolvedType::Function(Rc::new(ResolvedFunctionType {
+    #[test]
+    fn type_check_function() {
+        let res = type_check_program("let a = fn() -> int = 10", &SymbolTable::new()).unwrap();
+        let expected = ResolvedType::Function(Rc::new(ResolvedFunctionType {
             return_type: ResolvedType::Integer,
-            parameters: vec![ResolvedType::Integer],
-        })),
-        parameters: vec![],
-    }));
-    assert_eq!(expected, res.resolved_type);
-}
+            parameters: Vec::new(),
+        }));
+        assert_eq!(expected, res.resolved_type);
+    }
 
-#[test]
-fn test_check_function_call_args() {
-    let res = type_check_program("{ fn f(i:int)=i; f(true) }", &SymbolTable::new());
-    assert!(res.is_err());
+    #[test]
+    fn type_check_function_call() {
+        let res =
+            type_check_program("{ let a = fn() -> int = 10; a() }", &SymbolTable::new()).unwrap();
+        assert_eq!(ResolvedType::Integer, res.resolved_type);
+    }
+
+    #[test]
+    fn type_check_function_call_returning_function() {
+        let res = type_check_program(
+            "{ fn make_identity_fn() -> (int) -> int = fn(x: int) -> int = x }",
+            &SymbolTable::new(),
+        )
+        .unwrap();
+        let expected = ResolvedType::Function(Rc::new(ResolvedFunctionType {
+            return_type: ResolvedType::Function(Rc::new(ResolvedFunctionType {
+                return_type: ResolvedType::Integer,
+                parameters: vec![ResolvedType::Integer],
+            })),
+            parameters: vec![],
+        }));
+        assert_eq!(expected, res.resolved_type);
+    }
+
+    #[test]
+    fn test_check_function_call_args() {
+        let res = type_check_program("{ fn f(i:int)=i; f(true) }", &SymbolTable::new());
+        assert!(res.is_err());
+    }
 }
